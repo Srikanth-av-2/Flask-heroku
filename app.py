bde7d27ce71c4c840fd4480c8052db1cf6218d8d
Srikanth-av-1 <srikanth.av30@gmail.com>
2022-09-04 01:05:24 +0530

    fifth commit

--- app.py
+++ app.py
@@ -28,5 +28,4 @@ api.add_resource(UserRegister,'/register')
 api.add_resource(Store,'/store/<string:name>')
 api.add_resource(StoreList,'/stores/')
  
-if __name__=='__main__':
-    app.run(port=5000,debug=True)
+app.run(port=5000,debug=True)
