af5e370be85a829093a131ea6d0bc631c99c4283
Srikanth-av-1 <srikanth.av30@gmail.com>
2022-09-04 01:14:20 +0530

    sixth commit

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from mimetypes import init
 from flask import Flask
 from flask_restful import  Api
 from flask_jwt import JWT
@@ -6,18 +5,13 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'srikanth'
 api = Api(app)
-db.init_app(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity) #/auth
 
@@ -28,4 +22,7 @@ api.add_resource(UserRegister,'/register')
 api.add_resource(Store,'/store/<string:name>')
 api.add_resource(StoreList,'/stores/')
  
-app.run(port=5000,debug=True)
+if __name__=='__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000,debug=True)
